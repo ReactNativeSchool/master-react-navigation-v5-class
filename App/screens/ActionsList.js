9b114acf2df36da46a68eefa3ced3503628cce11
Spencer Carli <spencercarli@gmail.com>
2021-06-18 11:03:45 -0500

    Global Modal Screen

--- App/screens/ActionsList.js
+++ App/screens/ActionsList.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { View, Button } from "react-native";
 
-export default () => (
+export default ({ navigation }) => (
   <View style={{ flex: 1 }}>
-    <Button title="Open Modal" onPress={() => alert("todo!")} />
+    <Button title="Open Modal" onPress={() => navigation.navigate("Modal")} />
     <Button title="Open Alert" onPress={() => alert("todo!")} />
   </View>
 );
