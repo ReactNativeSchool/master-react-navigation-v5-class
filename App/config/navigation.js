90b7d004bb0c828a656db53d7ff86e49d4d20023
Spencer Carli <spencercarli@gmail.com>
2021-06-18 10:56:44 -0500

    Drawer Navigator: Basic Setup

--- App/config/navigation.js
+++ App/config/navigation.js
@@ -2,12 +2,14 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { Ionicons } from "@expo/vector-icons";
 
 import ContactsList from "../screens/ContactsList";
 import ContactDetails from "../screens/ContactDetails";
 import ActionsList from "../screens/ActionsList";
 import ActionDetails from "../screens/ActionDetails";
+import Settings from "../screens/Settings";
 
 const ContactsStack = createStackNavigator();
 const ContactsStackScreen = () => (
@@ -67,8 +69,16 @@ const AppTabsScreen = () => (
   </AppTabs.Navigator>
 );
 
+const AppDrawer = createDrawerNavigator();
+const AppDrawerScreen = () => (
+  <AppDrawer.Navigator>
+    <AppDrawer.Screen name="Tabs" component={AppTabsScreen} />
+    <AppDrawer.Screen name="Settings" component={Settings} />
+  </AppDrawer.Navigator>
+);
+
 export default () => (
   <NavigationContainer>
-    <AppTabsScreen />
+    <AppDrawerScreen />
   </NavigationContainer>
 );
