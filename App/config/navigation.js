0d8faa5c94bb5fa04a56a4746f2775de84ab07ad
Spencer Carli <spencercarli@gmail.com>
2021-06-18 11:02:40 -0500

    Authentication Setup

--- App/config/navigation.js
+++ App/config/navigation.js
@@ -10,6 +10,9 @@ import ContactDetails from "../screens/ContactDetails";
 import ActionsList from "../screens/ActionsList";
 import ActionDetails from "../screens/ActionDetails";
 import Settings from "../screens/Settings";
+import SignIn from "../screens/SignIn";
+import SignUp from "../screens/SignUp";
+import Loading from "../screens/Loading";
 
 const ContactsStack = createStackNavigator();
 const ContactsStackScreen = () => (
@@ -87,8 +90,34 @@ const AppDrawerScreen = () => (
   </AppDrawer.Navigator>
 );
 
-export default () => (
-  <NavigationContainer>
-    <AppDrawerScreen />
-  </NavigationContainer>
+const AuthStack = createStackNavigator();
+const AuthStackScreen = () => (
+  <AuthStack.Navigator>
+    <AuthStack.Screen name="SignIn" component={SignIn} />
+    <AuthStack.Screen name="SignUp" component={SignUp} />
+  </AuthStack.Navigator>
 );
+
+export default () => {
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(!isLoading);
+      setUser({});
+    }, 500);
+  }, []);
+
+  return (
+    <NavigationContainer>
+      {isLoading ? (
+        <Loading />
+      ) : user ? (
+        <AppDrawerScreen />
+      ) : (
+        <AuthStackScreen />
+      )}
+    </NavigationContainer>
+  );
+};
--- App/screens/SignIn.js
+++ App/screens/SignIn.js
@@ -1,1 +1,9 @@
-export default () => null;
+import React from "react";
+import { Button } from "react-native";
+
+export default ({ navigation }) => (
+  <>
+    <Button title="Sign In" onPress={() => alert("todo!")} />
+    <Button title="Sign Up" onPress={() => navigation.push("SignUp")} />
+  </>
+);
