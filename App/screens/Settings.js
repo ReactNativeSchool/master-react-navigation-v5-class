b8532b1840338ee55e4bac91e5b10f37b9ab76c9
Spencer Carli <1105429+spencercarli@users.noreply.github.com>
2022-03-08 13:42:35 -0600

    Drawer Navigator: Basic Setup

--- App/screens/Settings.js
+++ App/screens/Settings.js
@@ -1,1 +1,8 @@
-export default () => null;
+import React from "react";
+import { Button, SafeAreaView } from "react-native";
+
+export default ({ navigation }) => (
+  <SafeAreaView>
+    <Button title="Toggle Drawer" onPress={() => navigation.toggleDrawer()} />
+  </SafeAreaView>
+);
