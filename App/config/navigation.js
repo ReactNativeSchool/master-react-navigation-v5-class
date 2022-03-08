e042947a37a548a50e1f87eed1790cb8f5e021a6
Spencer Carli <1105429+spencercarli@users.noreply.github.com>
2022-03-08 13:37:48 -0600

    Tab Navigator: Basic Setup

--- App/config/navigation.js
+++ App/config/navigation.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import ContactsList from "../screens/ContactsList";
 import ContactDetails from "../screens/ContactDetails";
+import ActionsList from "../screens/ActionsList";
+import ActionDetails from "../screens/ActionDetails";
 
 const ContactsStack = createStackNavigator();
 const ContactsStackScreen = () => (
@@ -27,8 +30,24 @@ const ContactsStackScreen = () => (
   </ContactsStack.Navigator>
 );
 
+const ActionsStack = createStackNavigator();
+const ActionsStackScreen = () => (
+  <ActionsStack.Navigator>
+    <ActionsStack.Screen name="ActionsList" component={ActionsList} />
+    <ActionsStack.Screen name="ActionDetails" component={ActionDetails} />
+  </ActionsStack.Navigator>
+);
+
+const AppTabs = createBottomTabNavigator();
+const AppTabsScreen = () => (
+  <AppTabs.Navigator>
+    <AppTabs.Screen name="Contacts" component={ContactsStackScreen} />
+    <AppTabs.Screen name="Actions" component={ActionsStackScreen} />
+  </AppTabs.Navigator>
+);
+
 export default () => (
   <NavigationContainer>
-    <ContactsStackScreen />
+    <AppTabsScreen />
   </NavigationContainer>
 );
